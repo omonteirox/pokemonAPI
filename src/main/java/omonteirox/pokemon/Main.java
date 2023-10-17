90f88fa008e48994b71d24a346adce2d056c5c2e
Gustavo Monteiro <gussmonteiro@gmail.com>
2023-10-17 11:46:22 -0300

    Código comentado para importar os arquivos json para o banco

--- src/main/java/omonteirox/pokemon/Main.java
+++ src/main/java/omonteirox/pokemon/Main.java
@@ -4,6 +4,9 @@ package omonteirox.pokemon;
 import com.sun.jersey.api.container.grizzly2.GrizzlyServerFactory;
 import com.sun.jersey.api.core.PackagesResourceConfig;
 import com.sun.jersey.api.core.ResourceConfig;
+
+import omonteirox.pokemon.Utils.ImportJson;
+
 import org.glassfish.grizzly.http.server.HttpServer;
 
 import javax.ws.rs.core.UriBuilder;
@@ -40,6 +43,7 @@ public class Main {
     
     public static void main(String[] args) throws IOException {
         // Grizzly 2 initialization
+        // ImportJson.ImportarJSON("src/main/java/omonteirox/pokemon/Utils/pokedex.json");
         HttpServer httpServer = startServer();
         System.out.println(String.format("Jersey app started with WADL available at "
                 + "%sapplication.wadl\nHit enter to stop it...",
